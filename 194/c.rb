a6987f419fca3ff8824c12b547bfdff8ac25923c
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-03-25 22:25:50 +0900

    194c

--- 194/c.rb
+++ 194/c.rb
@@ -1,9 +1,16 @@
 N = gets.to_i
 A = gets.chomp.split.map(&:to_i)
 
+cnt = []
+0.upto(400) do |i|
+  cnt[i] = A.count(i-200)
+end
+
 sum = 0
-1.upto(A.length-1) do |i|
-  sum += i*(A[i]**2) - A[0]*i*A[i] - i*A[i]*A[i-1] + i*(A[0]**2)/2 + i*(A[i-1]**2)/2
+0.upto(400) do |i|
+  (i+1).upto(400) do |j|
+    sum += (i - j) ** 2 * cnt[i] * cnt[j]
+  end
 end
 
 puts sum

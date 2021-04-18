5c637a86d9a73c066976b89ef0c6bb940e5b3431
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-04-18 21:40:45 +0900

    readlineの追加

--- snipet.go
+++ snipet.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
+func readline() []string {
+	sc := bufio.NewScanner(os.Stdin)
+	var lines []string
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+	return lines
+}
+
 func max(a, b int) int {
 	if a > b {
 		return a

79b3b38114c4c37f81f702952248960139c3dd5d
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-07-11 14:17:45 +0900

    2分探索はheadとtailの移動による探索範囲の縮小。headとtailが上手く重ならなくとも隣り合うところまで来たら大丈夫

--- 205/d.rb
+++ 205/d.rb
@@ -1,49 +1,37 @@
 # いくつかWAがある
 n,q = gets.split.map(&:to_i)
 a = gets.chomp.split.map(&:to_i)
+a.unshift(0)
 good_num_counts = []
-good_num_counts << a[0] - 1
+good_num_counts << 0
 
-1.upto(n-1) do |i|
+1.upto(n) do |i|
   good_num_counts << good_num_counts[i-1] + (a[i] - a[i-1] - 1)
 end
 
+ans = []
 q.times do
   num = gets.to_i
   if num > good_num_counts[-1]
     # good_num_counts[-1]より大きい時
-    puts a[-1] + (num - good_num_counts[-1])
+    ans << a[-1] + (num - good_num_counts[-1])
 
   else
     # good_num_counts[-1]より小さい時
-    # num以上の最小値を探す。同値可
+    # num以下の最大値で、連続する場合一番右にあるもの
     # 計算量を考慮して2分探索
     head = 0
-    tail = n-1
-    min_max = -1
-    while true
-      if num > good_num_counts[(head+tail)/2]
-        head = (head+tail)/2
+    tail = n
+    until (head+1) == tail
+      check_point = (head+tail)/2
+      if num > good_num_counts[check_point]
+        head = check_point
       else
-        tail = (head+tail)/2
-      end
-
-      if head + 1 == tail
-        if good_num_counts[head] == good_num_counts[tail] || num == good_num_counts[head]
-          min_max = head
-        else
-          min_max = tail
-        end
-        break
+        tail = check_point
       end
     end
-
-    if min_max == -1
-      puts "error"
-      exit
-    end
-
-    puts (a[min_max]-1) - (good_num_counts[min_max] - num)
-
+    ans << a[head] + (num - good_num_counts[head])
   end
 end
+
+puts ans
